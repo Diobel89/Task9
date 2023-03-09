d688fc8a4b910a08dd6077c2c4c19490c5c70a09
Diobel89 <4510471diobel@gmail.com>
2023-03-09 22:26:02 +0100

    light implementation for SetNumberOfShips, added Fleet model.

---
+++ Functions/Simulation/Fleet.cs
@@ -0,0 +1,12 @@
+﻿using Task9.Models;
+
+namespace Task9.Functions.Simulation
+{
+    public class Fleet 
+    {
+        public List<Fleet> fleet1 = new List<Fleet>();
+        public List<Fleet> fleet2 = new List<Fleet>();
+        public Ship Ship { get; set; }
+        public int TotalDamage { get; set; }
+    }
+}
--- Functions/Simulation/Symulation.cs
+++ Functions/Simulation/Symulation.cs
@@ -1,34 +1,47 @@
-﻿namespace Task9.Functions.Simulation
+﻿using Task9.InputOutputSystem.Interface;
+
+namespace Task9.Functions.Simulation
 {
-    public class Symulation
+    public class Symulation : Fleet
     {
+        private readonly IInput input;
+        private readonly IOutput output;
+        public Symulation(IInput input, IOutput output)
+        {
+            this.input = input;
+            this.output = output;
+        }
         public void Run()
         {
-            Console.WriteLine("In development");
+            output.ShowMessage("In development");
         }
         private void GatherData()
         {
-            Console.WriteLine("In development");
+            output.ShowMessage("In development");
         }
         private void SetNumberOfShips()
         {
-            Console.WriteLine("In development");
+            int numberOfShips;
+            numberOfShips = input.GetIntValue("Podaj ilość statków na każdą z flot (np 20 = każda z flot będzie posiadałą po 20 okrętów)");
         }
-        private void SetShipType()
+        private void SetShipType(int numberOfShips)
         {
-            Console.WriteLine("In development");
+            for (int index = 0; index < numberOfShips * 2; index++)
+            {
+                output.ShowMessage("In development");
+            }
         }
         private void ChooceMap()
         {
-            Console.WriteLine("In development");
+            output.ShowMessage("In development");
         }
         private void Start()
         {
-
+            output.ShowMessage("In development");
         }
         private void End()
         {
-
+            output.ShowMessage("In development");
         }
     }
 }
